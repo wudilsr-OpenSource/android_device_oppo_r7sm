abb38aac4b37e972f204083a9803f14abe91957f
wudilsr <wudilsr@gmail.com>
2025-01-24 12:47:08 +0800

    r7sm: Add TARGET_OTA_ASSERT_DEVICE
    Some users still use twrp of r7sf.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ include device/oppo/msm8939-common/BoardConfigCommon.mk
 DEVICE_PATH := device/oppo/r7sm
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := r7s,r7sm,r7sm
+TARGET_OTA_ASSERT_DEVICE := r7s,r7sm,R7sm,r7sf,R7sf
 
 # Assertions
 TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
